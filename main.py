116ef3f057cf89862e19de411a12e31f9b6de7ab
Dmitry <babanov91@gmail.com>
2022-01-06 13:38:42 +0500

    v 0.3 (create JSON, create folder)

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-from pprint import pprint
+import json
 import requests
 from tqdm import tqdm
 from tqdm import tqdm_gui
-
 
 
 def get_token():
@@ -13,11 +12,10 @@ def get_token():
     return list_token
 
 
-def get_response():
+def get_response(id):
     url = 'https://api.vk.com/method/photos.get'
     params = {
-        # 'owner_id': 1,
-        'owner_id': 552934290,
+        'owner_id': id,
         'album_id': 'profile',
         'extended': 1,
         'rev': 0,
@@ -36,7 +34,7 @@ class VkPhoto:
         self.id = id
         self.url = vk_response.json()['response']['items'][id]['sizes'][-1]['url']
         self.size = vk_response.json()['response']['items'][id]['sizes'][-1]['type']
-        self.file_name = str(vk_response.json()['response']['items'][id]['likes']['count']) + '.txt'
+        self.file_name = str(vk_response.json()['response']['items'][id]['likes']['count']) + '.jpg'
 
 
 def create_photo_list(numbers):
@@ -48,12 +46,16 @@ def create_photo_list(numbers):
     return photo_list
 
 
-def upload_all_photo(list_photo):
+def upload_all_photo(list_photo, id):
     ya = YaUploader(token=yatoken)
+    data = []
     for i in tqdm(list_photo):
-    # for i in tqdm_gui(list_photo):    # вариант прогресс бара
-        ya.upload_file_from_url(eval(i).url, eval(i).file_name)
-    print("Success")
+        data.append({"file_name": eval(i).file_name, "size": eval(i).size})
+    # for i in tqdm_gui(list_photo):   # вариант прогресс бара
+        ya.upload_file_from_url(eval(i).url, eval(i).file_name, id)
+    with open('text.json', 'w') as file:
+        json.dump(data, file, indent=4)
+    print("Задача успешно завершена!")
 
 
 class YaUploader:
@@ -69,23 +71,34 @@ class YaUploader:
             "Host": "cloud-api.yandex.net"
         }
 
-    def upload_file_from_url(self, from_url, path_to):
-        params = {'path': path_to, 'url': from_url}
+    def create_folder(self, id):
+        path = 'id_'+str(id)
+        params = {'path': path}
+        response = requests.put(self.url+'resources', headers=self.get_headers(), params=params)
+        # response.raise_for_status()
+        if response.status_code == 200 or 201 or 202:
+            print(f'Директория "{path}", успешно создана')
+
+    def upload_file_from_url(self, from_url, path_to, id):
+        params = {'path': 'id_'+str(id)+'/'+path_to, 'url': from_url}
         response = requests.post(self.url+'resources/upload', headers=self.get_headers(), params=params)
         response.raise_for_status()
-        # if response.status_code == 200 or 201 or 202:
-        #     print("Success")
+        if response.status_code == 200:
+            print("Задача успешно выполнена")
         # pprint(response.json())
-        return response.json()
+        # return response.json()
 
 
 if __name__ == '__main__':
-    # photo_count = int(input('введите количтество фото'))
-    photo_count = 5
-    yatoken = get_token()[0].split()[-1]
+    yatoken = get_token()[0].split()[-1]  # Ya - токен. допускается раскоммитить переменную ниже, для ручного ввода
     vktoken = get_token()[1].split()[-1]
-    vk_response = get_response()
-    # TOKEN = input('Введите ваш токен: ')
+    persone_id = 1  # id Павла Дурова. допускается раскоммитить переменную ниже, для ручного ввода id персоны
+    photo_count = 5  # количество фото для загрузки. допускается раскоммитить переменную, для ручного ввода кол-ва фото
+    # photo_count = int(input('введите количество фото: '))
+    # persone_id = int(input('введите id персоны: '))
+    # yatoken = input('Введите ваш Yandex - токен: ')
+    vk_response = get_response(persone_id)
     ya = YaUploader(token=yatoken)
+    ya.create_folder(persone_id)
     photo_list = create_photo_list(photo_count)
-    upload_all_photo(photo_list)
+    upload_all_photo(photo_list, persone_id)
